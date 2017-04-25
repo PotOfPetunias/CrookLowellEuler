5dd7af7f28f972a26d8a1b1d982327711600158b
Lowell Crook <lhcrook@gmail.com>
2017-04-25 12:16:16 -0500

    Starting prob 7

--- python/CrookPE5_SmallestMod01-20.py
+++ python/CrookPE5_SmallestMod01-20.py
@@ -1,7 +1,6 @@
 #Find the smallest positive number that is
-#evenly divisible by all of the numbers from 1 to 20?
+#evenly divisible by all of the numbers from 1 to 20
 
-# work in progress
 import copy
 def findPrimeFactors(num):
     i = 2
@@ -32,8 +31,6 @@ def findSmallDivisible(numList):
         primeFactors.extend(numsFactors)
         #print('num: ', num, '\nFactors We need to add ', numsFactors, '\nNew Factor list ', primeFactors)
         
-        
-        
     answer = 1
     print(primeFactors)
     for num in primeFactors:
@@ -43,21 +40,3 @@ def findSmallDivisible(numList):
 print(findSmallDivisible([i for i in range(1,21)]))
 
 
-'''
-num = 2*3*5*7*11*13*17*19*2*2*3*5
-answer = num
-adding = 1
-for i in range(1,21):
-    if (num % i)!= 0:
-        print(num % i, " ", i)
-        if (adding % i)!= 0:
-            adding = adding*(num % i)
-print(answer)
-
-num = 2*3*5*7
-answer = num
-for i in range(1,11):
-    if (num % i)!= 0:
-        print(num % i, " ", i)
-        answer = answer*(num % i)
-print(answer)'''
---
+++ python/CrookPE7_10001Prime.py
@@ -0,0 +1,23 @@
+#Find the 10,001st prime number
+
+import math
+
+def primesUnder(num):
+    integerList = [i for i in range(0,num+1)]
+    markList = [False for i in range(0,num+1)]
+
+    for i in range(2,int(math.ceil(math.sqrt(num+1)))):
+        #print('Marking every ', i)
+        for j in range(i*2,num+1,i):
+            if not markList[j]:
+                #print('Marking ', integerList[j])
+                markList[j] = True
+                
+    for i in reversed(range(len(markList))):
+        if markList[i]:
+            del integerList[i]
+            
+    return integerList[2:]
+        
+    
+print(primesUnder(10))
