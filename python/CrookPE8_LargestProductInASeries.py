3243f10d7f6a94fc14853d04037811776bec564a
Lowell Crook <lhcrook@gmail.com>
2017-04-30 15:38:18 -0500

    Started Prob 8

---
+++ python/CrookPE8_LargestProductInASeries.py
@@ -0,0 +1,12 @@
+#Find the thirteen adjacent digits in
+#the 1000-digit number that have the greatest product
+
+import math
+import os
+
+os.chdir("C:\\Users\\lhcro\\Documents\\GitHub\\CrookLowellEuler")
+file = open("Prob8Number.txt")
+number = file.readlines()
+number = number[0]
+
+print(number)
